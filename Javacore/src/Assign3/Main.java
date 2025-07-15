a394a731aa16c4e06cb7998c8db0ac580f910495
subathra <subasubathra22903@gmail.com>
2025-07-15 21:25:07 +0530

    public

---
+++ Javacore/src/Assign3/Main.java
@@ -0,0 +1,15 @@
+package Assign3;
+
+public class Main {
+    public static void main(String[] args) {
+        Movie m1 = new Movie("Inception", "subathra");
+        Movie m2 = new Movie("The Matrix", "John", "Amaran", 136, 1999, "Action");
+
+        m1.acceptInfo("Christ", 148, 2010, "Sci-Fi");
+
+        m1.displayInfo();
+        m2.displayInfo();
+
+        System.out.println("Total Movies Created: " + Movie.getMoviesCount());
+    }
+}
